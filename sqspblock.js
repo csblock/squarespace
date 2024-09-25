e65b5a2dda6ee3a0d35fe47294f3dc09509fb864
csblock <153804872+csblock@users.noreply.github.com>
2024-09-25 11:21:30 +0600

    Update sqspblock.js

--- sqspblock.js
+++ sqspblock.js
@@ -1,1 +1,6 @@
-test
+// Add text to the webpage dynamically
+document.addEventListener('DOMContentLoaded', function() {
+    var element = document.createElement('p'); // Create a new <p> element
+    element.textContent = 'services'; // Set the text content
+    document.body.appendChild(element); // Append the element to the body of the document
+});
